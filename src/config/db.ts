739e2819c783b402cad664fbc7001bfe653a0f99
Harsh panwar <panwarharshwardhan@email.com>
2024-05-03 11:56:26 +0530

    connect DB to server

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,5 +2,18 @@ import mongoose from "mongoose";
 import { config } from "./config";
 
 const connectDB = async () => {
-  mongoose.connect(config.DB);
+  try {
+    await mongoose.connect(config.DB);
+    mongoose.connection.on("connected", () => {
+      console.log("Database connected successfully");
+    });
+    mongoose.connection.on("error", (error) => {
+      console.error("Database after connection error ", error);
+    });
+  } catch (error) {
+    console.error("Database connection error ", error);
+    process.exit(1);
+  }
 };
+
+export default connectDB;
